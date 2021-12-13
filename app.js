da57f16f748ca08760a0aa21e7bbe2458386f0bd
elliehastings <elliehastings@gmail.com>
2021-12-13 09:23:48 -0500

    Add ESLint for JS linting
    Uses eslint, prettier, eslint-config-prettier, and eslint-plugin-prettier
    (but not prettier-eslint) for formatting.
    
    May require corresponding setup in user or workspace settings to work
    with an editor.

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
 
 // Create a Bolt app
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 // All the room in the world for your code
